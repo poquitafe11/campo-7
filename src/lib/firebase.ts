6a4f33b51a5526d4de988db98a3d47b1478bbb93
marco leon <poquitafe_11@hotmail.com>
2025-07-20 19:14:52 +0000

    es increible que no te des cuenta de las cosas que falta configurar , ya

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,13 +5,12 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyBPoccPeNLmj1psrxCw5vMDJ5yrwPoITbk",
+  authDomain: "brujos.firebaseapp.com",
+  projectId: "brujos",
+  storageBucket: "brujos.firebasestorage.app",
+  messagingSenderId: "171849909417",
+  appId: "1:171849909417:web:0d594994387c214e5695b8"
 };
 
 // Initialize Firebase
