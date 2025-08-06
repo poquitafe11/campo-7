58dacc8a2bd0bb98e34904736a905a9e557f08de
marco leon <poquitafe_11@hotmail.com>
2025-08-06 04:44:28 +0000

    abajo tiene ese maldito scroll que ya te dije que no lo quiero ver y se

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -356,13 +356,11 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
   }
 
   return (
-    <div className="flex min-h-screen w-full flex-col bg-background">
-      <div className="flex flex-col flex-1">
-        <Header />
-        <main className="flex-1 p-4 sm:p-6 overflow-y-auto">
-          {children}
-        </main>
-      </div>
-    </div>
+    <>
+      <Header />
+      <main className="flex-1 p-4 sm:p-6 overflow-y-auto">
+        {children}
+      </main>
+    </>
   );
 }
