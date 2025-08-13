c32b5f66b351d16d2a6e9493d26bed5466cf0f9e
marco leon <poquitafe_11@hotmail.com>
2025-08-13 03:57:35 +0000

    nada todo sigue teniendo limites las tablas quita todo absolutamente tod

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -19,10 +19,12 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
   }
 
   return (
-    <div className="flex h-screen w-full bg-background">
+    <div className="w-full bg-background">
       <Sidebar />
-      <main className="flex-1 p-4 md:p-8 sm:ml-14 overflow-x-auto">
-        {children}
+      <main className="sm:ml-14">
+        <div className="overflow-x-auto p-4 md:p-8">
+            {children}
+        </div>
       </main>
     </div>
   );
