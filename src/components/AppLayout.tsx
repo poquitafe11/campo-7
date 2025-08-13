2c09b582be91a56e13d7865fd0d085d8530ee175
marco leon <poquitafe_11@hotmail.com>
2025-08-13 03:41:20 +0000

    necesito poder ver la tabla tal cual como se ve en la imagen de referenc

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -19,9 +19,9 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
   }
 
   return (
-    <div className="flex min-h-screen w-full bg-background">
+    <div className="flex h-screen w-full bg-background">
       <Sidebar />
-      <div className="flex flex-col flex-1">
+      <div className="flex flex-col flex-1 min-w-0">
         <main className="flex-1 p-4 md:p-8 sm:ml-14">
           {children}
         </main>
