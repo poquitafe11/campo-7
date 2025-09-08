da155015053da3b77da0d2d22e19084b09ec7aae
marco leon <poquitafe_11@hotmail.com>
2025-09-08 14:49:30 +0000

    Los cuadros están perfectos pero los números aún están arriba necesito q

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -10,7 +10,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
     return (
       <textarea
         className={cn(
-          "flex h-40 w-full items-center justify-center rounded-md border border-input bg-background px-3 py-2 text-center text-3xl font-bold ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+          "flex w-full rounded-md border border-input bg-background px-3 py-2 text-3xl font-bold text-center ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 h-24",
           className
         )}
         ref={ref}
