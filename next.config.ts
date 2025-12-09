39820f26589844c3bb27d316d806bd4f5cb5af68
marco leon <poquitafe_11@hotmail.com>
2025-12-09 21:49:48 +0000

    Me parece que la estas malogrado más ahora salio este problema

--- next.config.ts
+++ next.config.ts
@@ -1,50 +1,6 @@
 
 /** @type {import('next').NextConfig} */
 
-const withPWA = require('@ducanh2912/next-pwa').default({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-  register: true,
-  skipWaiting: true,
-  cacheOnFrontEndNav: true,
-  aggressiveFrontEndNavCaching: true,
-  runtimeCaching: [
-    {
-      urlPattern: ({ url }) => url.origin === self.origin,
-      handler: 'NetworkFirst',
-      options: {
-        cacheName: 'pages-cache',
-        networkTimeoutSeconds: 10,
-        expiration: {
-          maxEntries: 150,
-          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
-        },
-        cacheableResponse: {
-          statuses: [0, 200],
-        },
-      },
-    },
-    {
-      urlPattern: ({ url }) => url.origin !== self.origin,
-      handler: 'NetworkFirst',
-      options: {
-        cacheName: 'cross-origin-cache',
-        networkTimeoutSeconds: 10,
-        expiration: {
-          maxEntries: 50,
-          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
-        },
-        cacheableResponse: {
-          statuses: [0, 200],
-        },
-      },
-    },
-  ],
-  fallbacks: {
-    document: '/_offline',
-  }
-});
-
 const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
@@ -53,18 +9,8 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-      },
-       {
-        protocol: 'https',
-        hostname: 'unpkg.com',
-      }
-    ],
+    remotePatterns: [],
   },
 };
 
-module.exports = withPWA(nextConfig);
+module.exports = nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,9 +21,6 @@ export const metadata: Metadata = {
     template: `%s - ${APP_NAME}`,
   },
   description: APP_DESCRIPTION,
-  formatDetection: {
-    telephone: false,
-  },
   manifest: "/manifest.json",
   appleWebApp: {
     capable: true,
@@ -53,7 +50,6 @@ export default function RootLayout({
   return (
     <html lang="es" suppressHydrationWarning>
       <head>
-        <link rel="manifest" href="/manifest.json" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap" rel="stylesheet" />
